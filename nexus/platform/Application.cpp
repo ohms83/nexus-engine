b8e5fca24e0b322110ece6fc37f39753706f5aed
ohms <nuttachai@gumi.sg>
2021-10-20 20:21:22 +0800

    Revert "Fixed OpenGL's context creation failure (Note: context's attributes must be specified before creating window)"
    This reverts commit e63db2b14a1a5fdcf558b136461f1b4b6c21ce31.

--- nexus/platform/Application.cpp
+++ nexus/platform/Application.cpp
@@ -20,17 +20,6 @@ Application::~Application()
 
 void Application::init(const Info& info)
 {
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
     if( SDL_Init(SDL_INIT_EVERYTHING) < 0)
     {
         std::stringstream ss;
