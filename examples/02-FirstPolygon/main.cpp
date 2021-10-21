8352fa113af079530a72603c12d721ddd5654c1a
ohms <nuttachai@gumi.sg>
2021-10-21 19:06:02 +0800

    Example 02 - Updated to use GLShader class

--- examples/02-FirstPolygon/main.cpp
+++ examples/02-FirstPolygon/main.cpp
@@ -3,13 +3,32 @@
 
 #include "nexus.h"
 
+static const char *vertexShaderSource =
+    "#version 410 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 vertexColor;"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "    vertexColor = aColor;"
+    "}";
+
+static const char *fragmentShaderSource =
+    "#version 410 core\n"
+    "in vec3 vertexColor;\n"
+    "out vec4 FragColor;\n"
+    "void main() {\n"
+    "    FragColor = vec4(vertexColor, 1.0f);\n"
+    "}";
+
 class ExampleApp02 : public nexus::Application
 {
 protected:
     void onInit() override
     {
         createTriangle();
-        createShaderProgram();
+        _shader.initWithSource(vertexShaderSource, fragmentShaderSource);
     }
 
     void createTriangle()
@@ -61,88 +80,9 @@ protected:
         CHECK_GL_ERROR();
     }
 
-    void createVertexShader()
-    {
-        const char *source =
-            "#version 410 core\n"
-            "layout (location = 0) in vec3 aPos;\n"
-            "layout (location = 1) in vec3 aColor;\n"
-            "out vec3 vertexColor;"
-            "void main()\n"
-            "{\n"
-            "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-            "    vertexColor = aColor;"
-            "}";
-        
-        _vertexProgram = createShader(source, GL_VERTEX_SHADER);
-    }
-
-    void createFragmentShader()
-    {
-        const char *source =
-            "#version 410 core\n"
-            "in vec3 vertexColor;\n"
-            "out vec4 FragColor;\n"
-            "void main() {\n"
-            "    FragColor = vec4(vertexColor, 1.0f);\n"
-            "}";
-        
-        _fragmentProgram = createShader(source, GL_FRAGMENT_SHADER);
-    }
-
-    uint32_t createShader(const char* source, GLenum shaderType)
-    {
-        GLuint shader = glCreateShader(shaderType);
-        CHECK_GL_ERROR();
-        glShaderSource(shader, 1, &source, NULL);
-        CHECK_GL_ERROR();
-        glCompileShader(shader);
-        CHECK_GL_ERROR();
-        
-        int  success;
-        char infoLog[512];
-        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-        if(!success)
-        {
-            glGetShaderInfoLog(shader, 512, NULL, infoLog);
-            std::cout << "SHADER COMPILATION FAILED\n" << infoLog << std::endl;
-        }
-
-        return shader;
-    }
-
-    void createShaderProgram()
-    {
-        createVertexShader();
-        createFragmentShader();
-
-        _shaderProgram = glCreateProgram();
-        CHECK_GL_ERROR();
-        
-        glAttachShader(_shaderProgram, _vertexProgram);
-        CHECK_GL_ERROR();
-        glAttachShader(_shaderProgram, _fragmentProgram);
-        CHECK_GL_ERROR();
-        glLinkProgram(_shaderProgram);
-        CHECK_GL_ERROR();
-        
-        int  success;
-        char infoLog[512];
-        glGetProgramiv(_shaderProgram, GL_LINK_STATUS, &success);
-        if(!success) {
-            glGetProgramInfoLog(_shaderProgram, 512, NULL, infoLog);
-            std::cout << "SHADER PROGRAM CREATION FAILED\n" << infoLog << std::endl;
-        }
-        
-        glDeleteShader(_vertexProgram);
-        CHECK_GL_ERROR();
-        glDeleteShader(_fragmentProgram);
-        CHECK_GL_ERROR();
-    }
-
     void render(nexus::RenderSystem& renderSystem) override
     {
-        glUseProgram(_shaderProgram);
+        glUseProgram(_shader.getShaderProgram());
         CHECK_GL_ERROR();
         glBindVertexArray(_vao);
         CHECK_GL_ERROR();
@@ -151,7 +91,7 @@ protected:
     }
 private:
     nexus::VertexBuffer _vertexBuffer;
-    uint32_t _vertexProgram, _fragmentProgram, _shaderProgram;
+    nexus::GLShader _shader;
     uint32_t _vao, _vbo, _ebo;
 };
 
