896e7fb79a7488cb182ef27d1d0374651f91b730
ohms <ohms83@gmail.com>
2025-07-18 21:57:16 +0700

    [Build Tool] Fixing a bug that caused the header_tool.py not to run (WIP)

--- engine/src/Stub.cpp
+++ engine/src/Stub.cpp
@@ -2,3 +2,7 @@
 #define XXH_STATIC_LINKING_ONLY
 #define XXH_IMPLEMENTATION
 #include <xxhash.h>
+
+// This file exists only to force CMake to generate the header
+// It doesn't need to do anything else.
+#include <nexus/Nexus.h>
