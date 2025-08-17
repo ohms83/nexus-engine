5301cab45fead98dcfda6447fd4d93b42868ff48
ohms <ohms83@gmail.com>
2025-08-17 19:49:45 +0700

    [Example] Replaced hardcoded mesh data with CubeMesh in example 03 and 04

--- examples/example-03-Texture.cpp
+++ examples/example-03-Texture.cpp
@@ -1,84 +1,8 @@
 //
 // Created by nutta on 6/23/2025.
 //
-#include <nexus/Nexus.h>
-
-#include "nexus/scene/Transform.h"
+#include "nexus/Nexus.h"
 #include <glm/gtc/type_ptr.hpp>
-
-#include "nexus/graphics/Texture.h"
-
-struct Vertex
-{
-    glm::vec3 position;
-    glm::vec2 texCoord0;
-};
-
-// Vertices for a standard cube (24 vertices for 6 faces * 4 vertices/face)
-const std::vector<Vertex> cubeVertices = {
-    // Front face (Z+)
-    // Position             TexCoord
-    {{-0.5f, -0.5f,  0.5f}, {0.0f, 0.0f}}, // 0
-    {{ 0.5f, -0.5f,  0.5f}, {1.0f, 0.0f}}, // 1
-    {{ 0.5f,  0.5f,  0.5f}, {1.0f, 1.0f}}, // 2
-    {{-0.5f,  0.5f,  0.5f}, {0.0f, 1.0f}}, // 3
-    // Back face (Z-)
-    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f}}, // 4
-    {{ 0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}}, // 5
-    {{ 0.5f,  0.5f, -0.5f}, {0.0f, 1.0f}}, // 6
-    {{-0.5f,  0.5f, -0.5f}, {1.0f, 1.0f}}, // 7
-    // Top face (Y+)
-    {{-0.5f,  0.5f,  0.5f}, {0.0f, 1.0f}}, // 8 (same pos as 3)
-    {{ 0.5f,  0.5f,  0.5f}, {1.0f, 1.0f}}, // 9 (same pos as 2)
-    {{ 0.5f,  0.5f, -0.5f}, {1.0f, 0.0f}}, // 10 (same pos as 6)
-    {{-0.5f,  0.5f, -0.5f}, {0.0f, 0.0f}}, // 11 (same pos as 7)
-    // Bottom face (Y-)
-    {{-0.5f, -0.5f,  0.5f}, {0.0f, 0.0f}}, // 12 (same pos as 0)
-    {{ 0.5f, -0.5f,  0.5f}, {1.0f, 0.0f}}, // 13 (same pos as 1)
-    {{ 0.5f, -0.5f, -0.5f}, {1.0f, 1.0f}}, // 14 (same pos as 5)
-    {{-0.5f, -0.5f, -0.5f}, {0.0f, 1.0f}}, // 15 (same pos as 4)
-    // Right face (X+)
-    {{ 0.5f, -0.5f,  0.5f}, {0.0f, 0.0f}}, // 16 (same pos as 1)
-    {{ 0.5f, -0.5f, -0.5f}, {1.0f, 0.0f}}, // 17 (same pos as 5)
-    {{ 0.5f,  0.5f, -0.5f}, {1.0f, 1.0f}}, // 18 (same pos as 6)
-    {{ 0.5f,  0.5f,  0.5f}, {0.0f, 1.0f}}, // 19 (same pos as 2)
-    // Left face (X-)
-    {{-0.5f, -0.5f,  0.5f}, {1.0f, 0.0f}}, // 20 (same pos as 0)
-    {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f}}, // 21 (same pos as 4)
-    {{-0.5f,  0.5f, -0.5f}, {0.0f, 1.0f}}, // 22 (same pos as 7)
-    {{-0.5f,  0.5f,  0.5f}, {1.0f, 1.0f}}
-};
-
-static const std::vector<uint32_t> cubeIndices = {
-    // Front face (Z+): Vertices 0, 1, 2, 3. View from +Z. (BL, BR, TR, TL)
-    0, 3, 2,  // Triangle 1: Bottom-Left, Top-Left, Top-Right (CCW)
-    0, 2, 1,  // Triangle 2: Bottom-Left, Top-Right, Bottom-Right (CCW)
-
-    // Back face (Z-): Vertices 4, 5, 6, 7. View from -Z.
-    // Relative order when viewed from -Z: 6 (BL), 7 (BR), 4 (TR), 5 (TL)
-    6, 4, 5,  // Triangle 1: Bottom-Left, Top-Left, Top-Right (CCW)
-    6, 7, 4,  // Triangle 2: Bottom-Left, Bottom-Right, Top-Right (CCW)
-
-    // Top face (Y+): Vertices 8, 9, 10, 11. View from +Y.
-    // Relative order when viewed from +Y: 8 (BL), 9 (BR), 10 (TR), 11 (TL)
-    8, 11, 10, // Triangle 1: Bottom-Left, Top-Left, Top-Right (CCW)
-    8, 10, 9,  // Triangle 2: Bottom-Left, Top-Right, Bottom-Right (CCW)
-
-    // Bottom face (Y-): Vertices 12, 13, 14, 15. View from -Y.
-    // Relative order when viewed from -Y: 15 (BL), 14 (BR), 13 (TR), 12 (TL)
-    15, 12, 13, // Triangle 1: Bottom-Left, Top-Left, Top-Right (CCW)
-    15, 13, 14, // Triangle 2: Bottom-Left, Top-Right, Bottom-Right (CCW)
-
-    // Right face (X+): Vertices 16, 17, 18, 19. View from +X.
-    // Relative order when viewed from +X: 16 (BL), 17 (BR), 18 (TR), 19 (TL)
-    16, 19, 18, // Triangle 1: Bottom-Left, Top-Left, Top-Right (CCW)
-    16, 18, 17, // Triangle 2: Bottom-Left, Top-Right, Bottom-Right (CCW)
-
-    // Left face (X-): Vertices 20, 21, 22, 23. View from -X.
-    // Relative order when viewed from -X: 21 (BL), 20 (BR), 23 (TR), 22 (TL)
-    21, 22, 23, // Triangle 1: Bottom-Left, Top-Left, Top-Right (CCW)
-    21, 23, 20  // Triangle 2: Bottom-Left, Top-Right, Bottom-Right (CCW)
-};
 
 // Shader sources
 const char* vertexShaderSource = R"(
@@ -135,8 +59,8 @@ public:
         const nxs::RenderCommand renderCommand
         {
             m_shader,
-            m_vertexBuffer,
-            m_indexBuffer,
+            m_mesh->GetVertexBuffer(),
+            m_mesh->GetIndexBuffer(),
             {
                 {"model", m_cubeTransform.GetMatrix()},
                 {"view", view},
@@ -157,23 +81,6 @@ protected:
         const auto renderInterface = renderSystem.GetRenderInterface();
         renderSystem.SetClearColor(0x303030ff);
 
-        constexpr auto vertexSize = sizeof(Vertex);
-        const auto bufferSize = cubeVertices.size() * vertexSize;
-        m_vertexBuffer.reset(renderInterface->CreateVertexBuffer());
-        m_vertexBuffer->Begin()
-            .SetVertices(R_CAST<const uint8_t*>(cubeVertices.data()), bufferSize)
-            .SetUsage(nxs::BufferUsage::StaticDraw)
-            .AddAttribute(nxs::VertexAttribute {nxs::VertexAttribute::Type::Position, nxs::DataType::Float, 3})
-            .AddAttribute(nxs::VertexAttribute {nxs::VertexAttribute::Type::TexCoord0, nxs::DataType::Float, 2})
-        .Build();
-
-        m_indexBuffer.reset(renderInterface->CreateIndexBuffer());
-        m_indexBuffer->Begin()
-            .SetIndices(C_CAST<uint32_t*>(cubeIndices .data()), cubeIndices .size(), nxs::FrontFace::ClockWise)
-            .SetUsage(nxs::BufferUsage::StaticDraw)
-            .SetDrawMode(nxs::DrawMode::Triangle)
-        .Build();
-
         m_shader.reset(renderInterface->CreateShader());
         m_shader->BeginCompile()
             .AddSource(vertexShaderSource, nxs::Shader::Type::Vertex)
@@ -183,6 +90,8 @@ protected:
         m_texture = nxs::Engine::Instance().GetTextureManager()->Get(assetsPath);
         m_texture->SetWrapMode(nxs::TextureWrapMode::Clamp, nxs::TextureWrapMode::Clamp);
         m_texture->SetFiltering(nxs::TextureFilterMode::Linear, nxs::TextureFilterMode::Linear);
+
+        m_mesh = std::make_unique<nxs::CubeMesh>(renderInterface);
         return true;
     }
 
@@ -193,10 +102,9 @@ protected:
         m_camera.height = CAST<float>(screenSize.y);
     }
 
-    nxs::Ref<nxs::VertexBuffer> m_vertexBuffer;
-    nxs::Ref<nxs::IndexBuffer> m_indexBuffer;
     nxs::Ref<nxs::Shader> m_shader;
     nxs::Ref<nxs::Texture> m_texture;
+    nxs::Ptr<nxs::Mesh> m_mesh;
     nxs::Transform m_cubeTransform;
     nxs::CameraComponent m_camera;
 };
