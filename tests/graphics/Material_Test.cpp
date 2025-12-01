ea1c9f9369dd9e375c06446ae1dcf01decbb6b15
ohms <ohms83@gmail.com>
2025-12-01 12:47:25 +0700

    Tests: Refactor Material_Test to fix compilation; Tests: Add current source directory to nexus-test include paths

--- tests/graphics/Material_Test.cpp
+++ tests/graphics/Material_Test.cpp
@@ -49,34 +49,11 @@ TEST(MaterialTest, SerializeDeserialize)
 
     // Use test helper FakeRendering to create a TextureManager
     FakeRendering fakeR;
-    {
-    public:
-        FakeRendering() {}
-        ~FakeRendering() override {}
-        void ClearColor(const Color4F& color) override {}
-        void ClearDepth(float depth) override {}
-        void ClearBuffer(const Color4F& color, float depth) override {}
-        void SwapBuffer() override {}
-        void SetViewport(int32 x, int32 y, int32 w, int32 h) override {}
-        VertexBuffer* CreateVertexBuffer() const override { return nullptr; }
-        IndexBuffer* CreateIndexBuffer() const override { return nullptr; }
-        GpuProgram* CreateGpuProgram() const override { return nullptr; }
-        TextureProxy* CreateTexture() const override { return nullptr; }
-        void OnResize(uint32_t pixel_w, uint32_t pixel_h) override {}
-        void DrawIndexed(const Ref<IndexBuffer> indexBuffer) override {}
-        void DrawIndexedInstanced(const Ref<IndexBuffer> indexBuffer, uint32 instanceCount) override {}
-        PolygonMode GetPolygonMode() const override { return PolygonMode::Fill; }
-        void SetColorMask(const glm::bvec4& mask) override {}
-        void SetDepthMask(bool mask) override {}
-        void SetDepthFunction(DepthFunction depthFunction) override {}
-        void SetLineWidth(float width) override {}
-        void EnableDrawBuffer(DrawBuffer buffer) override {}
-    };
 
     auto renderingInterface = std::make_shared<FakeRendering>();
     TextureManager tm(renderingInterface);
     // Insert a dummy texture into the manager's cache by using Create (no loader needed)
-    auto tex = tm.Create("assets/tex.png");
+    tex = tm.Create("assets/tex.png");
     EXPECT_NE(tex, nullptr);
     m2.Resolve(tm, nullptr);
     // Since we did not add material to material manager, resolve will not populate m2 textures via material manager here.
